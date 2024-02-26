b3c59cf97a2f8908d4184860100d2034eef2baaf
recca0120 <recca0120@gmail.com>
2024-02-27 02:40:53 +0800

    chore

--- tests/Message/MyCash/PurchaseRequestTest.php
+++ tests/Message/MyCash/PurchaseRequestTest.php
@@ -16,11 +16,11 @@ class PurchaseRequestTest extends TestCase
     public function testCreditCardGetData(): void
     {
         $options = [
-            'MerTradeID' => '20151202001', // 店家交易編號(店家自行設定，不得小於 6 個字元，不得重複)
+            'transactionId' => '20151202001', // 店家交易編號(店家自行設定，不得小於 6 個字元，不得重複)
             'MerProductID' => 'sj6511', // 店家商品代號(店家自行設定，不得小於 4 個字元)
             'MerUserID' => 'Karl01', // 店家消費者 ID
-            'Amount' => '30', // 交易金額
-            'TradeDesc' => 'ItemDesc', // 交易描述
+            'amount' => '30', // 交易金額
+            'description' => 'ItemDesc', // 交易描述
             'ItemName' => 'ItemName', // 商品名稱
             'UnionPay' => '0', // 信用卡類別(0：一般信用卡；1：銀聯卡)
             'Installment' => '0', // 信用卡分期(未傳或非 1：不分期，1：分 3 期(分期消費金額最少 100 元))
@@ -71,7 +71,8 @@ class PurchaseRequestTest extends TestCase
             'ItemName' => 'ItemName',
         ], $request->getData());
 
-        self::assertEquals('https://api.mycash.asia/payment/VirAccountPaymentGate.php', $request->send()->getRedirectUrl());
+        self::assertEquals('https://api.mycash.asia/payment/VirAccountPaymentGate.php',
+            $request->send()->getRedirectUrl());
     }
 
     public function testCVSGetData(): void
